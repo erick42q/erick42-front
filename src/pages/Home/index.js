6e6cf281e354df3df6663afd74efe46ffb7e116e
Erick Queiroz de Faria <erick42q@gmail.com>
2022-09-29 05:35:49 -0300

    create routes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+import './styles.css'
+
+
+export default function Home() {
+    return (
+        
+        <div>
+            <h1>Pagina Home</h1>
+        </div>
+
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { BrowserRouter, Route, Routes  }  from 'react-router-dom'
+
+import Home from './pages/Home'
+
+export default function Rotas(){
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
