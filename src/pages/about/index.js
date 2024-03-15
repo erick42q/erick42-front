52a285bd56ec1a2c95be8c966201fba7b54007e6
Erick Queiroz <erick.qf.13@gmail.com>
2024-03-15 19:02:46 -0300

    add typeform to contact page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import { Widget } from '@typeform/embed-react'
+
 
 import './styles.css'
-
-
-
 
 export default function Contact({ user }) {
 
@@ -25,26 +24,17 @@ export default function Contact({ user }) {
 
                 <div className="row">
                     <div className="col-6 offset-3">
-                        {/* <h1>$ whoami</h1> */}
-                        <p></p>
-                        <p>
-                            Olá caro visitante,<br />
-                            Atualmente sou programador especialista em python e atuo com o framework django,<br />
-                            sou um Viking na <a href='https://gestaods.com.br' target="_blank" rel="noreferrer">GestãoDS</a> e estou sempre em busca de evolução,<br />
-                            criei esse espaço para expor alguns estudos e trabalhos que tenho feito ao longo da minha jornada.
-                        </p>
-                        <p>
-                            Meus cumprimentos
-                        </p>
-                        <br />
-                        <br />
-                        <h2>Contato:</h2>
-                        <p>
-                            erick42q@gmail.com<br />
-                            <a href='https://linkedin.com/in/erick42/'>https://linkedin.com/in/erick42/</a>
-                            <a aria-label="Chat on WhatsApp" href="https://wa.me/5561981786774?text=Ol%C3%A1,%20venho%20do%20seu%20site,%20tenho%20interesse%20de%20...
-"><img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.svg" /></a>
-                        </p>
+                        <h1>Contact:</h1>
+                        <br></br>
+                    </div>
+                    <div className="col-8 ">
+                        <Widget id="mQVoTwA3" style={{ width: '100%', height: '400px' }} className="my-form" />
+                    </div>
+                    <div className="col-4 align-self-center ">
+                        <a href='https://wa.me/5561981786774?text=Ol%C3%A1,%20venho%20do%20seu%20site,%20tenho%20interesse%20de%20...' target='blanck'>Whatsapp</a><br></br>
+                        <a href='https://linkedin.com/in/erick42/' target='blanck'>Linkedin</a><br></br>
+                        <a href='https://instagram.com/erick42q/' target='blanck'>instagram.com</a><br></br>
+                        <a href='https://github.com/erick42q/' target='blanck'>github</a>
                     </div>
                 </div>
             </div >
